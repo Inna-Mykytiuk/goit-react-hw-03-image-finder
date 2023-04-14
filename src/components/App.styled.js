68f76b88520f97bd48d9f5879b9ae1d37e4ef1fc
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-14 11:37:06 +0300

    update

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,17 +1,8 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  background-color: #d9dae0;
-  padding: 20px;
-  margin: 0 auto;
-  transition: 1.5s;
-
-  overflow-x: hidden;
-  color: #000;
-  background-color: #d9dae0;
+export const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
 `;
